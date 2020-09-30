bede4ffbc8c3cf41ab8156294f281aef8bb44990
borchicaD <borka.phoenix@gmail.com>
2020-09-30 13:53:02 +0200

    modal, localstorage just setItem...

--- main.js
+++ main.js
@@ -1,36 +1,72 @@
 var noteInput = $('.note');
 var noteRow = '';
-
-
-
-
+var timeInput = $('.timeInput');
 var db = [];
 
+	
+	//save note to localstorage
+function saveNotes(){
+	localStorage.setItem("notes",JSON.stringify(db));
+}
+//get note
+function getNotes(){
+	var noteFromSt = localStorage.getItem("notes"); 
+	db = JSON.parse(noteFromSt);
+	//console.log(db);
+	if(!db){
+		db=[];
+	}
+}
 
-$('.add-note').on('click', function(){
-	var newNote = noteInput.val();
-	console.log(newNote);
-	db.push(newNote);
-	console.log(db);
+
+function Note(note){
+    this.note = note;
+}
+
+function removeNote(index){
+	db.splice(index, 1);
+	saveNotes();
+}
+function getNote(index){
+	return db[index];
+}
+
+
+
+$('.add-note').on('click', function(note){
+	var note = noteInput.val();
+	//db.push(note);
+	
+	db.push(note);
+	//console.log(db);
+	var t= new Note(note);
+	saveNotes();
+	getNotes();
+	
 	
 	//adding note to the table
-	if(newNote != ''){
+	if(note != ''){
 		noteRow = '<tr>';
-		noteRow +='<td class="editNote">' + newNote + '</td>';
+		noteRow +='<td class="editNote">' + note + '</td>';
 		//noteRow +='<td><button class = "btn btn-info edit">Edit</button></td>';
-		noteRow +='<td><button class = "btn btn-warning timer">Add timer/reminder</button></td>';
+		
+		
+		//noteRow +='<td><button type="button" class="btn btn-warning">Timer/remainder</button></td>';
+		noteRow +='<td><button type="button" class="btn btn-warning remainder" data-toggle="modal" data-target="#myModal">Add remainder</button></td>';
+		
+		
 		noteRow +='<td><button class = "btn btn-success delete">Delete</button></td>';
 		noteRow +='</tr>';
 		$('.note-list tbody').append(noteRow);
 
-		
     //reset input
     noteInput.val('').focus();	
 	}else{
 		alert("Add a note..");
-	}
+	} 
+	
 
-    
+
 });
 
 
@@ -53,31 +89,14 @@ $('.startOver').on('click', function(){
 //edit content
 	$('tbody').on('click', '.editNote', function(){
 	$(this).attr("contenteditable", "true");
-
-	console.log("click");	
 });
 	
-
-
-
-
 	
-//timeBtn
-$('tbody').on('click', '.timer', function(){
-   	console.log("clik");
-});	
+//
+$('tbody').on('click', '.remainder', function(){
 	
-
-
-
-
-
-
-
-
-
-
-
+   console.log("click");
+});
 
 
 
